6590f8e7b5ea1a12f0dcaac047a7931524ab5597
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-09-25 15:13:27 +0900

    増やす＋引数と返り値変更

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,7 +2,11 @@ public class MyClass {
     public void bar() {
         System.out.println("Hello");
     }
-    public int add(int a, int y){
-        return a+y;
+    public int add(int a, int y, int x){
+        return a+y+x;
+    }
+
+    public int minus(int a, int y){
+        return a-y;
     }
 }
