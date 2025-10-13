e8d96ff2815b1a8243671c7058448daf6b47007f
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-10-13 16:53:25 +0900

    Update MyClass.java

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,11 +1,5 @@
 public class MyClass {
-    public void bar() {
-        System.out.println("Hello");
-    }
-    public int add(int a, int y, int x){
-        return a+y+x;
-    }
-
+    
     public int minus(int a, int y){
         return a-y;
     }
