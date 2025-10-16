0c1b563b55be3514c87c1a4a554ae4a0f029cb4d
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-10-16 13:52:47 +0900

    Update Add.java

--- src/Add.java
+++ src/Add.java
@@ -1,6 +1,14 @@
 public class Add { 
+  
   public int ppulas(int a, int b) {
       return a*b;
+  }
+  private double sub(int a, int b int c) {
+        return a - b;
+  }
+
+  private double sub2(int a, int b) {
+        return a - b -5;
   }
 }
 
