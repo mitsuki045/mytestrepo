745c82d66c47a18e20975f73a7c7679881762c8b
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-10-16 16:02:10 +0900

    Update Add.java

--- src/main/java/com/example/Add.java
+++ src/main/java/com/example/Add.java
@@ -5,7 +5,7 @@ public class Add {
   public int newplus(int a, int b) {
       return a*b;
   }
-  private int sub(int a, int b,int c) {
+  private int newsub(int a, int b,int c) {
         return a - b;
   }
 
