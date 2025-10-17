25472338480f7b3a9adbc0bef894a5d89c91bde0
Mitsuki <mitsuki.morishita01@gmail.com>
2025-10-17 15:48:53 +0900

    コミットメッセージ（例：2つのJavaファイルを変更）

--- src/main/java/client/Add.java
+++ src/main/java/client/Add.java
@@ -2,11 +2,9 @@ package client;
 
 public class Add { 
   
-  public int newplus(int a, int b) {
-      return a*b;
-  }
+  
 
-  public int minus(int a, int b){
+  protected int minus(int a, int b){
       return a-b;   
   }
 
--- src/main/java/client/MyClass.java
+++ src/main/java/client/MyClass.java
@@ -5,4 +5,8 @@ public class MyClass {
     protected int newminuss(int a, int y){
         return a-y;
     }
+
+    public int newplus(int a, int b) {
+      return a*b;
+  }
 }
