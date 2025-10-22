fd2b2ff79260658cf448678f71512bd83b758960
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-10-23 03:33:29 +0900

    Update And.java

--- src/main/java/client/And.java
+++ src/main/java/client/And.java
@@ -3,7 +3,7 @@ package client;
 public class And { 
 
   
-  protected int newadd(int a, int b){
+  protected int add(int a, int b){
       int result = a * 2 + b / 3 - c * 4;
       result += (a + b) * (c - 2);
       result = result % 5 + (b * b) / (a + 1);
