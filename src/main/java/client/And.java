c2eacb7cca5757ad45eec32c3e4fd0e611e7e2fa
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-10-23 17:23:39 +0900

    Update And.java

--- src/main/java/client/And.java
+++ src/main/java/client/And.java
@@ -3,7 +3,7 @@ package client;
 public class And { 
 
   
-  public int add(int a, int b, int c){
+  public static int add(int a, int b, int c){
       int result = a * 2 + b / 3 - c * 4;
       result += (a + b) * (c - 2)+10;
       result = result % 5 + (b * b) / (a + 1);
