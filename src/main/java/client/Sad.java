c9868bd70696743a1db36855dce26bfff3f24894
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-10-23 15:56:29 +0900

    Update and rename And.java to Sad.java

--- src/main/java/client/Sad.java
+++ src/main/java/client/Sad.java
@@ -1,13 +1,13 @@
 package client;
 
-public class And { 
+public class Sad { 
 
   
   public int add(int a, int b, int c){
       int result = a * 2 + b / 3 - c * 4;
       result += (a + b) * (c - 2)+10;
       result = result % 5 + (b * b) / (a + 1);
-      return result+100;
+      return result;
   }
 }
 
