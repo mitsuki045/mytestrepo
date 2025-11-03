4c41f99ecbc1f20e862c4ff729aed89c5c32f44e
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-11-03 22:36:41 +0900

    Update AndCaller.java

--- src/main/java/client/AndCaller.java
+++ src/main/java/client/AndCaller.java
@@ -2,27 +2,8 @@ package client;
 
 public class AndCaller {
 
-    public void main(String[] args) {
-        // Andクラスのインスタンスを生成
-        And and = new And();
-
-        // addメソッドを呼び出す（staticではないのでインスタンス経由）
-        int result1 = and.add(1, 2, 3);
-        int result2 = and.add(5, 10, 2);
-        int result3 = and.add(0, 4, 1);
-
-        System.out.println("結果1: " + result1);
-        System.out.println("結果2: " + result2);
-        System.out.println("結果3: " + result3);
-
-        // 条件付きの利用例（依存関係を示すループ）
-        for (int i = 0; i < 3; i++) {
-            int a = i + 1;
-            int b = i * 2 + 3;
-            int c = 5 - i;
-            int res = and.add(a, b, c);
-            System.out.println("ループ呼び出し " + i + ": add(" + a + "," + b + "," + c + ") = " + res);
-        }
+    public int tekito(int a, int b){
+        return a+b;
     }
-}
 
+}
