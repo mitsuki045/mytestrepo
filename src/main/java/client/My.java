9a6b810796f8a39f81b49fde6736929f70708a09
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-11-17 23:54:21 +0900

    Update My.java

--- src/main/java/client/My.java
+++ src/main/java/client/My.java
@@ -5,7 +5,7 @@ public class My {
     public int newplus(int a, int b) {
       return a*b;
   }
-    public static int minus(int a, int b){
+    public static int newminus(int a, int b){
       return a-b;   
   }
 
