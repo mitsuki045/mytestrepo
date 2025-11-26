8ccaed2f0c5ed6829c7914e4bf00b8ad7a51e11b
Mitsuki <mitsuki.morishita01@gmail.com>
2025-11-26 19:16:37 +0900

    コミットメッセージ（例：2つのJavaファイルを変更）

--- src/main/java/client/AndCaller.java
+++ src/main/java/client/AndCaller.java
@@ -2,11 +2,11 @@ package client;
 
 public class AndCaller {
 
-    public int newtekito(int a, int b){
+    public static int newtekito(int a, int b){
         return a+b;
     }
 
-    public int minuss(int a, int y){
+    public static int minuss(int a, int y){
         return a-y;
     }
 
