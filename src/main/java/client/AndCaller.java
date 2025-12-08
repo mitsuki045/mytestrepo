0ec6cd295823d7e9a6a725c447e4a74accea760b
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-12-08 11:53:44 +0900

    Update AndCaller.java

--- src/main/java/client/AndCaller.java
+++ src/main/java/client/AndCaller.java
@@ -2,10 +2,6 @@ package client;
 
 public class AndCaller {
 
-    public static int newtekito(int a, int b){
-        return a+b;
-    }
-
     public static int minuss(int a, int y){
         return a-y;
     }
