6e1c3e004092b28f7812c8cab2b0780896cccb7b
Mitsuki <mitsuki.morishita01@gmail.com>
2025-12-12 18:02:53 +0900

    hukusuuhenka

--- src/main/java/client/And.java
+++ src/main/java/client/And.java
@@ -1,7 +1,7 @@
 package client;
 
 public class And { 
-  public int newadd(int a, int b, int c){
+  public int add(int a, int b, int c){
       int result = a * 2 + b / 3 - c * 4;
       result += (a + b) * (c - 2)+10;
       result = result % 5 + (b * b) / (a + 1);
--- src/main/java/client/My.java
+++ src/main/java/client/My.java
@@ -3,7 +3,7 @@ package client;
 public class My {
 
     public static int minus(int a, int b){
-      return a-b;   
+      return a-b*100-1000+b;   
   }
 
 }
