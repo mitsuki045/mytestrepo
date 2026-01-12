7be5b123b35913b3c35a5fae7ccc51a4383be435
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2026-01-12 16:53:58 +0900

    Update Calculator.java

--- src/main/java/example/Calculator.java
+++ src/main/java/example/Calculator.java
@@ -1,6 +1,6 @@
-Packege example;
-Public class Calculator{
-        public int newadd(int a,int b){
-		      return a+b+1;
+packege example;
+public class Calculator{
+        public int add(int a,int b){
+		      return a+b;
 	}
 }
